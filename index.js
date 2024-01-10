990be0860a2809b98f893f02a3ed9729928f45c2
anthony <anthonyefrisca@gmail.com>
2024-01-10 03:54:10 -0500

    Rewrite playRound function
    Function returning improper results; original kept in comments

--- index.js
+++ index.js
@@ -9,26 +9,53 @@ function getComputerChoice() {
     return random
 }
 
+// function playRound(rps) {
+// // All losing scenarios
+// console.log(getComputerChoice())
+//     if (getComputerChoice() === scissors && rps === paper) {
+//         console.log('You lose! Scissors beat paper!') }
+//     if (getComputerChoice() === rock && rps === scissors) {
+//         console.log('You Lose! Rock beats scissors!') }
+//     if (getComputerChoice() === paper && rps === rock) {
+//         console.log('You Lose! Paper beats rock!') }
+// // All draw scenarios
+//     if (getComputerChoice() === paper && rps === paper) {
+//         console.log('Draw!') }
+//     if (getComputerChoice() === scissors && rps === scissors) {
+//         console.log('Draw!') }
+//     if (getComputerChoice() === rock && rps === rock) {
+//         console.log('Draw!') }
+// // All winning scenatios
+//     if (getComputerChoice() === scissors && rps === rock) {
+//         console.log('You Win! Rock beats scissors!') }
+//     if (getComputerChoice() === rock && rps === paper) {
+//         console.log('You Win! Paper beats rock!') }
+//     if (getComputerChoice() === paper && rps === scissors) {
+//         console.log('You Win! Scissors beat paper!') }
+// }
+
 function playRound(rps) {
-// All losing scenarios
-    if (getComputerChoice() == scissors && rps == paper) {
-        console.log('You lose! Scissors beat paper!') }
-    if (getComputerChoice() == rock && rps == scissors) {
-        console.log('You Lose! Rock beats scissors!') }
-    if (getComputerChoice() == paper && rps == rock) {
-        console.log('You Lose! Paper beats rock!') }
-// All draw scenarios
-    if (getComputerChoice() == paper && rps == paper) {
-        console.log('Draw!') }
-    if (getComputerChoice() == scissors && rps == scissors) {
-        console.log('Draw!') }
+    console.log(getComputerChoice())
+
     if (getComputerChoice() == rock && rps == rock) {
-        console.log('Draw!') }
-// All winning scenatios
-    if (getComputerChoice() == scissors && rps == rock) {
-        console.log('You Win! Rock beats scissors!') }
-    if (getComputerChoice() == rock && rps == paper) {
-        console.log('You Win! Paper beats rock!') }
-    if (getComputerChoice() == paper && rps == scissors) {
-        console.log('You Win! Scissors beat paper!') }
+        console.log('Draw!')
+        }else if (rps == paper) {
+            console.log('You win! Paper beats rock!')
+        }else if (rps == scissors) {
+            console.log('You lose! Rock beats scissors!')
+        }
+   if (getComputerChoice() == paper && rps == paper) {
+        console.log('Draw!')
+        }else if (rps == rock) {
+            console.log('You lose! Paper beats rock!')
+        }else if (rps == scissors) {
+            console.log('You win! Scissors beat paper!')
+        }
+   if (getComputerChoice() == scissors && rps == scissors) {
+        console.log('Draw!')
+        }else if (rps == paper) {
+            console.log('You lose! Scissors beat paper!')
+        }else if (rps == rock) {
+            console.log('You win! Rock beats scissors!')
+        }
 }
