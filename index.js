18fedec9578c5cbf1a5e0cd7dcfb5ee2ced8906b
anthony <anthonyefrisca@gmail.com>
2024-01-11 07:24:49 -0500

    Seperate conditions to locate problem
    Breaking code down into smaller chunks to find problem with execution

--- index.js
+++ index.js
@@ -14,16 +14,26 @@ function playRound() {
     console.log(rps);
     let choiceVar = getComputerChoice();
     console.log(choiceVar);
-    if (choiceVar == rock && rps == rock || choiceVar == paper
-    && rps == paper || choiceVar == scissors && rps == scissors) {
+    
+    const isDrawRock = choiceVar === rock && rps === rock
+    const isDrawPaper = choiceVar === paper && rps === paper 
+    const isDrawScissors = choiceVar === scissors && rps === scissors
+
+    const youWinRock = choiceVar === scissors && rps === rock
+    const youWinPaper = choiceVar === rock && rps === paper
+    const youWinScissors = choiceVar === paper && rps === scissors
+
+    const youLoseRock = choiceVar === paper && rps === rock
+    const youLosePaper = choiceVar === scissors && rps === paper
+    const youLoseScissors = choiceVar === rock && rps === scissors
+
+    if (isDraw) {
         console.log('Draw! Try again!')
         return 'Draw! Try again!' }
-    if (choiceVar == rock && rps == scissors || choiceVar == paper
-    && rps == rock || choiceVar == scissors && rps == paper) {
+    if (youLose) {
         console.log('You lose!')
         return 'You lose!' }
-    if (choiceVar == rock && rps == paper || choiceVar == paper
-    && rps == scissors || choiceVar == scissors && rps == rock) {
+    if (youWin) {
         console.log('You win!')
         return 'You win!'}
 }
