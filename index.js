f11fccf8b5bc2ffcd08ca3aa2b66d3e7f7e69f76
anthony <anthonyefrisca@gmail.com>
2024-01-11 03:33:03 -0500

    Make prompt case insensitive
    Code is not functioning but does convert to case insensitive

--- index.js
+++ index.js
@@ -9,16 +9,21 @@ function getComputerChoice() {
     return random
 }
 
-function playRound(rps) {
+function playRound() {
+    let rps = prompt().toLowerCase();
+    console.log(rps);
     let choiceVar = getComputerChoice();
     console.log(choiceVar);
     if (choiceVar == rock && rps == rock || choiceVar == paper
     && rps == paper || choiceVar == scissors && rps == scissors) {
-        console.log('Draw!') }
+        console.log('Draw! Try again!')
+        return 'Draw! Try again!' }
     if (choiceVar == rock && rps == scissors || choiceVar == paper
     && rps == rock || choiceVar == scissors && rps == paper) {
-        console.log('You lose!') }
+        console.log('You lose!')
+        return 'You lose!' }
     if (choiceVar == rock && rps == paper || choiceVar == paper
     && rps == scissors || choiceVar == scissors && rps == rock) {
-        console.log('You win!')}
+        console.log('You win!')
+        return 'You win!'}
 }
