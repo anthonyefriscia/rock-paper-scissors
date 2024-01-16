971c5e061dae55dfbf0ab7ac4ef69172f49d643e
anthony <anthonyefrisca@gmail.com>
2024-01-16 07:31:30 -0500

    Add game() function and boolean
    Answers that trigger this boolean will ask you to try again

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const rock = "Rock"
 const paper = "Paper"
 const scissors = "Scissors"
+const isDraw = choiceVar === rock && rps === 'rock' || choiceVar === paper
+&& rps === 'paper' || choiceVar === scissors && rps === 'scissors';
+const youWin = choiceVar === scissors && rps === 'rock' || choiceVar === 
+rock && rps === 'paper' || choiceVar === paper && rps === 'scissors';
+const youLose = choiceVar === paper && rps === 'rock' || choiceVar === 
+scissors && rps === 'paper' || choiceVar === rock && rps === 'scissors';
 
 // This function performs the computer half of the RPS game
 function getComputerChoice() {
@@ -17,18 +23,41 @@ function playRound() {
     
     const isDraw = choiceVar === rock && rps === 'rock' || choiceVar === paper
     && rps === 'paper' || choiceVar === scissors && rps === 'scissors';
+
     const youWin = choiceVar === scissors && rps === 'rock' || choiceVar === 
     rock && rps === 'paper' || choiceVar === paper && rps === 'scissors';
+
     const youLose = choiceVar === paper && rps === 'rock' || choiceVar === 
     scissors && rps === 'paper' || choiceVar === rock && rps === 'scissors';
 
+    if (rps = isDraw || youLose || youWin) {
+        noAnswer = false}
+        else {noAnswer = true }
     if (isDraw) {
         console.log('Draw! Try again!')
-        return 'Draw! Try again!' }
+        return 'Draw! Try again!'; }
     if (youLose) {
         console.log('You lose!')
         return 'You lose!' }
     if (youWin) {
         console.log('You win!')
-        return 'You win!'}
+        return 'You win!'}   
+    if (noAnswer) {
+        console.log('Not an answer!')
+        return 'Not an answer!'
+ }
+}
+
+
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+    
+
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+
 }
