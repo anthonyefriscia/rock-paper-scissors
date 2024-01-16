03b117b503e95d2f0ef946cd34ea36bc855b5cc3
anthony <anthonyefrisca@gmail.com>
2024-01-16 07:53:35 -0500

    Remove most console logs
    Simple as that

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const rock = "Rock"
 const paper = "Paper"
 const scissors = "Scissors"
-const isDraw = choiceVar === rock && rps === 'rock' || choiceVar === paper
-&& rps === 'paper' || choiceVar === scissors && rps === 'scissors';
-const youWin = choiceVar === scissors && rps === 'rock' || choiceVar === 
-rock && rps === 'paper' || choiceVar === paper && rps === 'scissors';
-const youLose = choiceVar === paper && rps === 'rock' || choiceVar === 
-scissors && rps === 'paper' || choiceVar === rock && rps === 'scissors';
+let playerScore = 0
+let computerScore = 0
+
 
 // This function performs the computer half of the RPS game
 function getComputerChoice() {
@@ -17,9 +14,7 @@ function getComputerChoice() {
 
 function playRound() {
     let rps = prompt().toLowerCase();
-    console.log(rps);
     let choiceVar = getComputerChoice();
-    console.log(choiceVar);
     
     const isDraw = choiceVar === rock && rps === 'rock' || choiceVar === paper
     && rps === 'paper' || choiceVar === scissors && rps === 'scissors';
@@ -34,16 +29,12 @@ function playRound() {
         noAnswer = false}
         else {noAnswer = true }
     if (isDraw) {
-        console.log('Draw! Try again!')
         return 'Draw! Try again!'; }
     if (youLose) {
-        console.log('You lose!')
         return 'You lose!' }
     if (youWin) {
-        console.log('You win!')
         return 'You win!'}   
     if (noAnswer) {
-        console.log('Not an answer!')
         return 'Not an answer!'
  }
 }
@@ -52,6 +43,7 @@ function playRound() {
 function game() {
     let playerScore = 0
     let computerScore = 0
+
     
 
     playRound();
@@ -59,5 +51,17 @@ function game() {
     playRound();
     playRound();
     playRound();
-
+    if (playRound() == 'You lose!') {
+        let computerScore = computerScore +1
+        console.log(playerScore, computerScore)
+    }
+    if (playRound() == 'You win!') {
+        let playerScore = playerScore +1
+        console.log(playerScore, computerScore)
+    }
+    if (playRound() == 'Draw! Try again!' || 'Not an answer!') {
+        playRound();
+    }
+    console.log('Your Score: ' + playerScore )
+    console.log('Computer Score: ' + computerScore )
 }
